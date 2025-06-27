f70bf7b43e5d5ac8e9b10d9076cad584ce8c1403
2991692032 <cizq297@gmail.com>
2025-06-27 23:42:50 +0800

    修复bug

--- src/main/java/com/bengebackend/controller/RoomController.java
+++ src/main/java/com/bengebackend/controller/RoomController.java
@@ -6,11 +6,13 @@ import com.bengebackend.entity.room.applyRoomEntity;
 import com.bengebackend.entity.room.createRoomEntity;
 import com.bengebackend.entity.room.getAllRoomEntity;
 import com.bengebackend.service.RoomService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
