13ff73cc31e0236cdf1cac5a498d23edfd344202
2991692032 <cizq297@gmail.com>
2025-07-02 10:47:51 +0800

    房间列表只能查询出人数未满的房间

--- src/main/java/com/bengebackend/service/serviceImpl/RoomServiceImpl.java
+++ src/main/java/com/bengebackend/service/serviceImpl/RoomServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoomServiceImpl implements RoomService {
@@ -26,7 +27,7 @@ public class RoomServiceImpl implements RoomService {
         // 计算分页偏移量
         int offset = (getAllRoomsEntity.getPage() - 1) * getAllRoomsEntity.getLimit();
         // 查询房间列表
-        List<RoomDto> roomDtos = roomMapper.getAllRooms(offset, getAllRoomsEntity.getLimit());
+        List<RoomDto> roomDtos = roomMapper.getAllRooms(offset, getAllRoomsEntity.getLimit()).stream().filter(room->room.getCurrentMembers()<4).collect(Collectors.toList());
 
         return roomDtos;
     }
