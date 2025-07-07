1ca4e3f25941433c71b3693d46331ef7f4dccee7
chantouRichard <2897167455@qq.com>
2025-07-07 14:56:04 +0800

    解决一些bug——陈涛7.7

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     proxy: {
       '/api': {
         // target: 'https://9cd1-2001-250-4001-5012-c1e1-eff4-a331-25f4.ngrok-free.app',
-        target: 'http://localhost:7122',
+        target: 'https://just-croatia-misc-intake.trycloudflare.com',
         changeOrigin: true,
         secure: false,//
         pathRewrite: { '^/api': '/api' }
